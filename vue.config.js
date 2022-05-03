022709cc48944e0cd4d7481b77106ce2ba08d149
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2022-05-03 23:41:14 +0300

    +++

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Vue-Todo/'
-    : '/'
-}
+  productionSourceMap: false,  
+  outputDir: '../dist',
+  assetsDir: 'static',
+  indexPath: 'templates/index.html'
+};
